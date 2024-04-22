196bb8595383c42e26c388195237435aededf26c
Shafosphere <117680528+Shafosphere@users.noreply.github.com>
2024-04-22 19:48:44 +0200

    Starting the app

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 8080;
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+app.use(cors(corsOptions));
+app.get('/api/data', (req, res) => {
+    // Handle your API logic here
+    const data = { message: 'Is this working?' };
+    res.json(data);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
