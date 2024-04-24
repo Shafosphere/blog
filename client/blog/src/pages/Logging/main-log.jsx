c8ea61cf833e56fd8e7f9a33e950f8959d7b10d0
Shafosphere <117680528+Shafosphere@users.noreply.github.com>
2024-04-24 15:32:47 +0200

    Added database connection, passwords and username

--- client/blog/src/pages/Logging/main-log.jsx
+++ client/blog/src/pages/Logging/main-log.jsx
@@ -1,20 +1,41 @@
 import "./main-log.css";
 import LoginForm from "../../components/Logging/Login/LoginForm";
+import { useState } from "react";
 export default function Logging({ onLogin }) {
+  const [display, setDisplay] = useState("login");
   return (
     <div className="container-logging">
       <div className="window-logg">
-        <div className="top-logg">
-          <LoginForm onLogin={onLogin} />
-        </div>
-        <div className="bot-logg">
-          <button className="button" type="submit">
-            register
-          </button>
-          <button className="button" type="submit">
-            reset
-          </button>
-        </div>
+        {display === "login" && (
+          <>
+            <div className="top-logg">
+              <LoginForm onLogin={onLogin} />
+            </div>
+            <div className="bot-logg">
+              <button className="button" type="submit">
+                register
+              </button>
+              <button className="button" type="submit">
+                reset
+              </button>
+            </div>
+          </>
+        )}
+        {display === "register" && (
+          <>
+            <div className="top-logg">
+              <LoginForm onLogin={onLogin} />
+            </div>
+            <div className="bot-logg">
+              <button className="button" type="submit">
+                register
+              </button>
+              <button className="button" type="submit">
+                reset
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 import express from "express";
 import session from 'express-session';
 import cors from "cors";
+import pg from "pg";
 
 const app = express();
 const port = 8080;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "blog",
+  password: "",
+  port: 5433,
+});
+db.connect();
 
 app.use(express.json());
 app.use(session({
@@ -19,19 +29,30 @@ const corsOptions = {
     credentials: true,
 };
 
-const users = {
-    'admin': 'admin'
-};
-
 app.use(cors(corsOptions));
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    if (users[username] && users[username] === password) {
-      req.session.user = username;
-      res.status(200).send('successful login');
-    } else {
-      res.status(401).send('Invalid username or password');
+    try{
+      const result = await db.query("SELECT * FROM users WHERE username = $1", [
+        username,
+      ]);
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        const storedPassword = user.password;
+
+        if(password === storedPassword){
+          req.session.user = username;
+          res.status(200).send('successful login');
+        } else {
+          res.status(401).send('Invalid username or password');
+        }
+      } else{
+        res.status(404).send("User not found");
+      }
+    } catch (err) {
+      res.status(500).send(err.message);
+      console.log(err);
     }
 });
 
