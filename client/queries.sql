52abb33086fb7b91daa1027c812b9d968e158bc2
Shafosphere <117680528+Shafosphere@users.noreply.github.com>
2024-05-02 16:04:07 +0200

    TABLEs SQL

--- client/queries.sql
+++ client/queries.sql
@@ -4,11 +4,17 @@ CREATE TABLE users(
   email VARCHAR(100) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL
 )
-CREATE TABLE articles(
+CREATE TABLE articles (
   id SERIAL PRIMARY KEY,
-  author_id INT NOT NULL UNIQUE,
-  author_username VARCHAR(100) NOT NULL UNIQUE,
-  
-
-  
+  author_id INT NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(255),
+  content TEXT NOT NULL,
+  image_id INT, 
+  creation_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE images(
+  id SERIAL PRIMARY KEY,
+  image_path TEXT,
+  is_local BOOLEAN NOT NULL 
 )
